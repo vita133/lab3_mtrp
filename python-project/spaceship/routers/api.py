2d204d826503e345d3303a19ee5db5455d5d8c9b
vita133 <vitandrukhiv@gmail.com>
2023-04-02 21:19:55 +0300

    add matrix endpoint, alpine build

--- python-project/spaceship/routers/api.py
+++ python-project/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
@@ -6,3 +7,15 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get('/matrix')
+def multiply_matrices() -> dict:
+    matrix_a = numpy.random.rand(10, 10).tolist()
+    matrix_b = numpy.random.rand(10, 10).tolist()
+    product = numpy.matmul(matrix_a, matrix_b).tolist()
+    return {
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": product
+    }
